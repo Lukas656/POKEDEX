10615cdc5d859f7e6d332d05d2fb07f95daf89bb
Lukas656 <le.lu.ka2018@gmail.com>
2024-03-06 11:39:31 -0300

    css do Jogo

--- src/data/cards.js
+++ src/data/cards.js
@@ -49,6 +49,56 @@ const Cards = [
     url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/472.png",
     alt: "Gliscor",
   },
+  {
+    id: 11,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+    alt: "Charizard",
+  },
+  {
+    id: 12,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png",
+    alt: "Pidgeot",
+  },
+  {
+    id: 13,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png",
+    alt: "Beedrill ",
+  },
+  {
+    id: 14,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png",
+    alt: "Meowth ",
+  },
+  {
+    id: 15,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png",
+    alt: "Psyduck",
+  },
+  {
+    id: 16,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/074.png",
+    alt: "Geodude",
+  },
+  {
+    id: 17,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/130.png",
+    alt: "Gyarados",
+  },
+  {
+    id: 18,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png",
+    alt: "Eevee",
+  },
+  {
+    id: 19,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png",
+    alt: "Dragonite ",
+  },
+  {
+    id: 20,
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/157.png",
+    alt: "Typhlosion",
+  },
 ];
 
 export default Cards;
